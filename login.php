f49e9905f361dc43b54e40a11986c3ededc55271
mayileshwaran <mayileshwaran2005@gmail.com>
2025-07-25 21:16:09 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -16,10 +16,10 @@
         <h2>Create Account</h2>
         <input type="text" name="signup_name" placeholder="Name" required />
         <input type="email" name="signup_email" placeholder="Email" required />
-        <div class="input-wrapper">
-  <input type="password" id="login_password" name="login_password" placeholder="Password" required />
-  <i class="fa fa-eye toggle-password" onclick="togglePassword(this, 'login_password')"></i>
-</div>
+        <div class="password-field">
+          <input type="password" name="signup_password" id="signup_password" placeholder="Password" required />
+          <i class="fa fa-eye toggle-password" onclick="togglePassword('signup_password', this)"></i>
+        </div>
         <button name="signup_btn" type="submit">Sign Up</button>
       </form>
     </div>
@@ -31,11 +31,10 @@
         <input type="email" id="login_email" name="login_email" placeholder="Email" required />
         <div id="emailError" class="error-message"></div>
 
-       <div class="input-wrapper" >
-  <input type="password" id="signup_password" name="signup_password" placeholder="Password" required />
-  <i class="fa fa-eye toggle-password" onclick="togglePassword(this, 'signup_password')"></i>
-</div>
-
+        <div class="password-field">
+          <input type="password" id="login_password" name="login_password" placeholder="Password" required />
+          <i class="fa fa-eye toggle-password" onclick="togglePassword('login_password', this)"></i>
+        </div>
         <div id="passwordError" class="error-message"></div>
 
         <button name="login_btn" type="submit">Login</button>
